7309fc42a81e4578a736f037e45e47c4809d5012
Divam <dfordivam@gmail.com>
2024-11-15 17:07:10 +0900

    Move scroller* out from implementation.rs

--- rust-app/src/implementation.rs
+++ rust-app/src/implementation.rs
@@ -1,8 +1,6 @@
 use crate::interface::*;
 use crate::settings::*;
-use crate::test_parsers::*;
 use crate::ui::*;
-use crate::utils::*;
 use alamgu_async_block::*;
 use arrayvec::ArrayVec;
 use ledger_crypto_helpers::common::{try_option, Address, CryptographyError};
@@ -102,38 +100,18 @@ pub async fn sign_apdu(io: HostIO, settings: Settings) {
     let known_txn = false;
 
     if known_txn {
-        if final_accept_prompt(&["Sign Transaction?"]).is_none() {
-            reject::<()>(StatusWords::UserCancelled as u16).await;
-        };
+        // NYI
     } else if !settings.get_blind_sign() {
-        scroller("WARNING", |w| {
-            Ok(write!(
-                w,
-                "Transaction not recognized, enable blind signing to sign unknown transactions"
-            )?)
-        });
+        warn_tx_not_recognized();
         reject::<()>(SyscallError::NotSupported as u16).await;
     } else {
-        if scroller("WARNING", |w| Ok(write!(w, "Transaction not recognized")?)).is_none() {
-            reject::<()>(StatusWords::UserCancelled as u16).await;
-        }
-
-        if scroller("Transaction hash", |w| Ok(write!(w, "{}", hash.deref())?)).is_none() {
-            reject::<()>(StatusWords::UserCancelled as u16).await;
-        }
-
         if with_public_keys(&path, false, |_, pkh: &PKH| {
-            scroller("Sign for Address", |w| Ok(write!(w, "{pkh}")?))
-                .ok_or(CryptographyError::NoneError)
+            confirm_sign_tx(pkh, hash.deref()).ok_or(CryptographyError::NoneError)
         })
         .is_err()
         {
             reject::<()>(StatusWords::UserCancelled as u16).await;
         }
-
-        if final_accept_prompt(&["Blind Sign Transaction?"]).is_none() {
-            reject::<()>(StatusWords::UserCancelled as u16).await;
-        };
     }
 
     // By the time we get here, we've approved and just need to do the signature.
--- rust-app/src/ui/nano.rs
+++ rust-app/src/ui/nano.rs
@@ -1,9 +1,7 @@
 use core::fmt::Write;
+use ledger_crypto_helpers::eddsa::Ed25519RawPubKeyAddress;
+use ledger_crypto_helpers::hasher::Base64Hash;
 use ledger_prompts_ui::*;
-use ledger_crypto_helpers::eddsa::{
-    Ed25519RawPubKeyAddress,
-};
-use ledger_crypto_helpers::hasher::{Base64Hash};
 
 pub fn confirm_address(pkh: &Ed25519RawPubKeyAddress) -> Option<()> {
     scroller("Provide Public Key", |_w| Ok(()))?;
@@ -12,11 +10,19 @@ pub fn confirm_address(pkh: &Ed25519RawPubKeyAddress) -> Option<()> {
 }
 
 pub fn confirm_sign_tx(pkh: &Ed25519RawPubKeyAddress, hash: &Base64Hash<32>) -> Option<()> {
-    scroller("Transaction hash", |w| {
-        Ok(write!(w, "{}", hash)?)
-    })?;
+    scroller("WARNING", |w| Ok(write!(w, "Transaction not recognized")?))?;
+    scroller("Transaction hash", |w| Ok(write!(w, "{}", hash)?))?;
     scroller("Sign for Address", |w| Ok(write!(w, "{pkh}")?))?;
-    final_accept_prompt(&["Sign Transaction?"])
+    final_accept_prompt(&["Blind Sign Transaction?"])
+}
+
+pub fn warn_tx_not_recognized() {
+    scroller("WARNING", |w| {
+        Ok(write!(
+            w,
+            "Transaction not recognized, enable blind signing to sign unknown transactions"
+        )?)
+    });
 }
 
 #[cfg(not(target_os = "nanos"))]
